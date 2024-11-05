bb2d4c104850602e610289e46f81ad028c6d11ee
CramY2001 <szymek.kramarczyk@gmail.com>
2024-11-05 02:16:35 +0100

    Update transportujFlota.hpp

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -1,9 +1,21 @@
 #pragma once
-
 #include "Stocznia.hpp"
-
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar == 0) return 0;
+    Stocznia Gdansk;
+    unsigned int TransportSuma =0;
+    unsigned int LiczbaZaglowcow=0;
+    while (TransportSuma < towar && TransportSuma <=Stocznia::getTotalCap())
+    {
+        Statek* statek= Gdansk();
+        unsigned int transport = statek->transportuj();
+        TransportSuma += transport;
+        if (dynamic_cast<Zaglowiec*>(statek))
+        {
+            ++LiczbaZaglowcow;
+        }
+    delete statek;
+    }
+return LiczbaZaglowcow;
 }
